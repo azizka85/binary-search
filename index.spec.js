774c28c73f960ddaa87c0f06e0594705c53f1167
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2022-11-05 21:52:22 +0600

    fix

--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,30 @@
 const { binarySearch, upperBound, lowerBound } = require('./index');
+
+describe('Binary Search test', () => {
+  test('Test binarySearch method', () => {
+    let a = [1, 23, 7, 98, 34, 54, 2];
+
+    a.sort((a, b) => a - b);
+
+    expect(binarySearch(a, 23)).toEqual(3);
+    expect(binarySearch(a, 24)).toEqual(null);
+  });
+
+  test('Test upperBound method', () => {
+    let a = [1, 23, 23, 23, 7, 98, 34, 54, 2];
+
+    a.sort((a, b) => a - b);
+
+    expect(upperBound(a, 23)).toEqual(6);
+    expect(upperBound(a, 24)).toEqual(6);
+  });
+
+  test('Test lowerBound method', () => {
+    let a = [1, 23, 23, 23, 7, 98, 34, 54, 2];
+
+    a.sort((a, b) => a - b);
+
+    expect(lowerBound(a, 23)).toEqual(3);
+    expect(lowerBound(a, 24)).toEqual(6);
+  });
+});
